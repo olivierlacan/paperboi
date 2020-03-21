79eec0edfd56c10e132bb0a13b06a72e58e048d9
Olivier Lacan <hi@olivierlacan.com>
2020-03-21 01:33:17 -0400

    Return list of URLs instead of just one
    Co-authored-by: Casey Faist <caseylfaist@gmail.com>

--- paperboi.rb
+++ paperboi.rb
@@ -7,7 +7,7 @@ class Paperboi
                                               language: 'en',
                                               country: 'us')
 
-    top_headlines.filter { _1.title.match?("death") }.collect { "<a href='#{_1.url}'>#{_1.title}</a>"}.first
+    top_headlines.filter { _1.title.match?("death") }.collect { "<a href='#{_1.url}'>#{_1.title}</a>"}.join("\n")
   end
 
   def self.api
