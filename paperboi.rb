8bb938888bb5537dc3c0a4e14df2ccea31e2e3e3
Olivier Lacan <hi@olivierlacan.com>
2020-03-21 04:38:35 -0400

    Extract request building logic

--- paperboi.rb
+++ paperboi.rb
@@ -1,17 +1,28 @@
 require 'news-api'
+require 'date'
 
 class Paperboi
-  def self.news(query)
-    top_headlines = api.get_top_headlines(q: query,
-                                              category: 'health',
-                                              language: 'en',
-                                              country: 'us')
+  def self.news(query, state)
+    request_query = build_request(query, state)
 
-    top_headlines.collect { "<li><a href='#{_1.url}'>#{_1.title}</a></li>"}.join("\n")
+  def self.build_request(query, state)
+    {
+      qInTitle: "#{query} AND #{state}",
+      from: from_date, to: to_date,
+      language: 'en', sortBy: 'relevancy', pageSize: 1
+    }
   end
 
   def self.api
     @newsapi ||= News.new(ENV["NEWS_API_KEY"])
+  end
+
+  def self.from_date
+    Date.today.prev_day(3).iso8601
+  end
+
+  def self.to_date
+    Date.today.iso8601
   end
   def self.production?
     ENV["RACK_ENV"] == "production"
